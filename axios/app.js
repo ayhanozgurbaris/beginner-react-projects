82ea5964b62bcf7f6d89e41a2da3fc04c25df7ec
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-07-31 18:01:27 +0300

    packages added

--- axios/app.js
+++ axios/app.js
@@ -1,2 +1,14 @@
-import axios from "axios";
+const axios = require('axios');
 
+async function getData(Number) {
+     
+    Number = 1;
+    
+    axios.get(`https://jsonplaceholder.typicode.com/users/${Number}`).then(resp => {
+  
+        console.log(resp.data);
+    });
+
+    }
+    
+getData()
